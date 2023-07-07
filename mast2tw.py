54469968b73efb53399edb24ba03edf062aafc46
mpwwww <111195758+mpwwww@users.noreply.github.com>
2023-07-07 08:31:39 -0400

    image function

--- mast2tw.py
+++ mast2tw.py
@@ -19,6 +19,11 @@ processed_file = "processed_statuses.txt"
 
 date_str = str(today)
 
+# twitter api keys
+consumer_key=os.environ['CONSUMER_KEY']
+consumer_secret=os.environ['CONSUMER_SECRET']
+access_token=os.environ.get('ACCESS_TOKEN')
+access_token_secret=os.environ.get('ACCESS_TOKEN_SECRET')
 
 #timestamp of a specified date
 def get_timestamp(date_str):
@@ -58,14 +63,55 @@ def htmlToText(status_html):
 
     # 替换链接元素为纯文本形式
     for link in soup.find_all('a'):
-#         print(link['href'])
-        link.replace_with(link.get_text()+' '+link['href'])
+        url = link['href']
+        if "https://mas.to/tags" in url: # remove link of tags
+            link.replace_with(link.get_text())
+        else: 
+            link.replace_with(link.get_text()+' '+link['href']) 
 
     # 提取纯文本内容
     status_text = soup.get_text()
     return status_text
 
-def tweet(content): 
+def get_status_images(status:list) -> list:
+    """
+    获取状态中的图片
+    `status` is a status's json file fetched from mastodon
+    Reutn: a list of images' url and id, or "no attachment"
+    """
+    images = []
+    if image == []:
+        return "no attachment"
+    for image in status["media_attachments"]:
+        try:
+            url = image['url']
+            id = image['id']
+            print(url)
+            images.append({'url':url,'id':id})
+            return images
+        except:
+            pass   
+   
+def download_an_image(image:dict) -> None:
+    """
+    下载图片
+    `image` is a dict, which contains url and id of image.
+    """
+    response = requests.get(image['url'])
+    id = image['id']
+    if response.status_code == 200:
+        image_file = response.content
+        # name the image file with id
+        with open(f"{id}.jpg", "wb") as f:
+            f.write(image_file)
+    else:
+        print("status code: ",response.status_code)
+        raise Exception("下载图片失败")
+
+
+
+
+def tweet(content, images:list): 
 
     client = tweepy.Client(
         consumer_key=os.environ['CONSUMER_KEY'],
@@ -76,7 +122,31 @@ def tweet(content):
 
     # send tweet 
     client.create_tweet(text=content)
+    if images != []:        
+        # upload the file using api v1.1
+        auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+        auth.set_access_token(access_token, access_token_secret)
+        api = tweepy.API(auth) 
+        
+        # download images
+        medias = []
+        for image in images:
+            download_an_image(image)
+            filename = f"{image['id']}.jpg"
+            media = api.media_upload(filename) # upload the file
+            medias.append(media.media_id_string) # append id  
+        
+        # send tweet with images
+        client.create_tweet(text=content, media_ids=medias)
+       
+        # delete the image file
+        for image in images:
+            os.remove(f"{image['id']}.jpg")
 
+
+            
+
+            
 
 def latest_status_id(status):
     pass
@@ -119,8 +189,9 @@ statuses = statuses[::-1] #reverse the statuses list
 for status in statuses:
     content = status["content"]
     statusId= status["id"]
+    images = get_status_images(status)
     if not is_status_processed(statusId): 
-        tweet(htmlToText(content))
+        tweet(content, images)
         print("状态内容:", htmlToText(content))
         mark_status_processed(statusId)
         print("---") 
